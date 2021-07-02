d3ad89efe348fa36a68c76ede7d307494795ee41
Ronald <ronald.patrick1101@gmail.com>
2021-07-03 00:09:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Products from "./screens/Products";
+import AddProduct from "./screens/AddProduct"
+import ProdDetails from "./screens/ProdDetails";
+import Profile from "./screens/Profile";
+import Homepage from "./screens/Homepage";
+import Auth from "./screens/Auth";
+import {auth} from './firebase'
+import { useState } from "react";
+import Navbarr from "./components/Navbar";
+import Cart from "./screens/Cart"
 
 function App() {
+	const [user,setUser]=useState(JSON.parse(localStorage.getItem('user')));
+	const signOut=()=>{
+	  auth.signOut().then(()=>{
+		localStorage.removeItem('user');
+		setUser(null);
+	  })
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	  <>
+    <Switch>
+
+	{
+		!user ?
+		<Auth setUser={setUser}/>
+		:
+		<>
+		<Navbarr user={user.userType} signOut={signOut} />
+		<Route exact path="/" component={Homepage} />
+      <Route exact path="/products" component={Products} />
+	  <Route path="/products/:state" component={Products} />
+	  <Route path="/addProduct" component={AddProduct} />
+	  <Route path="/details/:id" component={ProdDetails} />
+	  <Route path="/profile" component={Profile} />
+	  <Route path="/cart" component={Cart} />
+
+		</>
+	}
+
+    </Switch>
+	</>
   );
 }
 
